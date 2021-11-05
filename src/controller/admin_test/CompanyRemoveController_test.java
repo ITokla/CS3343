75deda0a46e362f2759a953d7df8898e43d8bd87
ITokla <ltlctokla@gmail.com>
2021-11-05 20:38:51 +0800

    test

--- src/controller/admin_test/CompanyRemoveController_test.java
+++ src/controller/admin_test/CompanyRemoveController_test.java
@@ -39,7 +39,8 @@ public class CompanyRemoveController_test {
 		
 		assertEquals("Not found.\r\n", stream.toString());
 	}
-	
+	public void execute_test2() {
+	}
 //	@Test
 //	public void execute_test2() {
 //		class CompanyRemoveView2 extends CompanyRemoveView{
