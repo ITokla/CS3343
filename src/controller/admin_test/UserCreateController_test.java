b23ea475abd28ee010565ce15692049cd7c78507
ryan11222005 <ryan11222005@gmail.com>
2021-11-12 17:20:38 +0800

    2021.11.12 17:18 Test UserCreateController, the "usernameDuplicate" cannot test because of the while loop

---
+++ src/controller/admin_test/UserCreateController_test.java
@@ -0,0 +1,157 @@
+package controller.admin_test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import org.junit.jupiter.api.Test;
+
+import controller.admin.ReportController;
+import controller.admin.UserCreateController;
+import system.MCRB;
+import view.CommandView;
+import view.UserCreateView;
+
+class UserCreateController_test {
+	
+	public ByteArrayOutputStream getOutputStream() {
+		ByteArrayOutputStream stream = new ByteArrayOutputStream();
+		PrintStream printStream = new PrintStream(stream);
+		System.setOut(printStream);
+		return stream;
+	}
+	
+	@Test
+	void test_createEmployee_companyExist() {
+		class testUserCreateView extends UserCreateView{
+			public testUserCreateView(Scanner input) {
+				super(input);
+			}
+			public String getUsername() {
+				return "tc01";
+			}
+			public String getCompanyName() {
+				return "cmp1";
+			}
+			public int selectList(ArrayList<?> list) {
+				return 0;
+			}
+		}
+		class testUserCreateController extends UserCreateController{
+			public testUserCreateController(UserCreateView view) {
+				super(view);
+			}
+		}
+		Scanner input = new Scanner(System.in);
+		ByteArrayOutputStream stream = getOutputStream();
+
+		testUserCreateView tucv = new testUserCreateView(input);
+		testUserCreateController ucc = new testUserCreateController(tucv);
+		ucc.execute();
+		
+		boolean result = (MCRB.getInstance().searchEmployee("tc01") != null);
+		assertEquals(true,result);
+		
+	}
+	
+	@Test
+	void test_createEmployee_companyExist_invalidIndex() {
+		class testUserCreateView extends UserCreateView{
+			public testUserCreateView(Scanner input) {
+				super(input);
+			}
+			public String getCompanyName() {
+				return "cmp1";
+			}
+			public int selectList(ArrayList<?> list) {
+				return 1;
+			}
+		}
+		class testUserCreateController extends UserCreateController{
+			public testUserCreateController(UserCreateView view) {
+				super(view);
+			}
+		}
+		Scanner input = new Scanner(System.in);
+		ByteArrayOutputStream stream = getOutputStream();
+
+		testUserCreateView tucv = new testUserCreateView(input);
+		testUserCreateController ucc = new testUserCreateController(tucv);
+		ucc.execute();
+		
+		assertEquals("",stream.toString());
+		
+	}
+	
+//	@Test
+//	void test_createEmployee_companyExist_userDuplicate() {
+//		class testUserCreateView extends UserCreateView{
+//			public testUserCreateView(Scanner input) {
+//				super(input);
+//			}
+//			public String getUsername() {
+//				return "cp1test1";
+//			}
+//			public String getCompanyName() {
+//				return "cmp1";
+//			}
+//			public int selectList(ArrayList<?> list) {
+//				return 0;
+//			}
+//		}
+//		class testUserCreateController extends UserCreateController{
+//			public testUserCreateController(UserCreateView view) {
+//				super(view);
+//			}
+//		}
+//		Scanner input = new Scanner(System.in);
+//		ByteArrayOutputStream stream = getOutputStream();
+//
+//		testUserCreateView tucv = new testUserCreateView(input);
+//		testUserCreateController ucc = new testUserCreateController(tucv);
+//		ucc.execute();
+//		
+//		assertEquals("Username is already\r\n",stream.toString());
+		
+//	}
+
+	@Test
+	void test_createEmployee_companyNotExist() {
+		class testUserCreateView extends UserCreateView{
+			public testUserCreateView(Scanner input) {
+				super(input);
+			}
+			public String getCompanyName() {
+				return "cmp0";
+			}
+		}
+		class testUserCreateController extends UserCreateController{
+			public testUserCreateController(UserCreateView view) {
+				super(view);
+			}
+		}
+		Scanner input = new Scanner(System.in);
+		ByteArrayOutputStream stream = getOutputStream();
+		
+		testUserCreateView tucv = new testUserCreateView(input);
+		testUserCreateController ucc = new testUserCreateController(tucv);
+		ucc.execute();
+		
+		assertEquals("Company not found\r\n",stream.toString());
+		
+	}
+	
+	@Test
+	void test_generateReport_getDescription() {
+		Scanner input = new Scanner(System.in);
+		UserCreateView ucv = new UserCreateView(input);
+		UserCreateController uc = new UserCreateController(ucv);
+		String result = uc.getDescription();
+		assertEquals("Create Employee", result);	
+	}
+
+}
